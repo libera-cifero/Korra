dcb4b77ea1a38caa9668d7721b1737c1c92da3fb
libera-cifero <jhoenotbro@proton.me>
2026-07-10 22:19:38 +0300

    ffmpeg_rtmp_pipe_in

--- include/encoder/provider/video_pipe/ffmpeg_rtmp/ffmpeg_rtmp_pipe_in.hpp
+++ include/encoder/provider/video_pipe/ffmpeg_rtmp/ffmpeg_rtmp_pipe_in.hpp
@@ -5,6 +5,7 @@
 class ffmpeg_rtmp_pipe_in : public video_pipe_in {
 private:
     ffmpeg_rtmp_config _config;
+    FILE *_pipe_in;
 public:
     ffmpeg_rtmp_pipe_in(ffmpeg_rtmp_config config);
     uint8_t *read(int size) override;
--- src/encoder/provider/video_pipe/ffmpeg_rtmp/ffmpeg_rtmp_pipe_in.cpp
+++ src/encoder/provider/video_pipe/ffmpeg_rtmp/ffmpeg_rtmp_pipe_in.cpp
@@ -1,13 +1,19 @@
 #include "encoder/provider/video_pipe/ffmpeg_rtmp/ffmpeg_rtmp_pipe_in.hpp"
+#include <cstdio>
 
 ffmpeg_rtmp_pipe_in::ffmpeg_rtmp_pipe_in(ffmpeg_rtmp_config config){
     _config = config;
+    char command[256];
+    sprintf(command, "ffmpeg -i %s -f rawvideo -pix_fmt bgr24 -an pipe:1", config.rtmp_url.c_str());
+    _pipe_in = popen(command, "r");
 }
 
 uint8_t *ffmpeg_rtmp_pipe_in::read(int size) {
-
+    uint8_t *data=new uint8_t[size];
+    fread(data, 1, size, _pipe_in);
+    return data;
 }
 
 ffmpeg_rtmp_pipe_in::~ffmpeg_rtmp_pipe_in(){
-
+    pclose(_pipe_in);
 }
