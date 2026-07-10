d49f9486d8440ac4985ca95bd9d1e01dbaa5e9e0
libera-cifero <jhoenotbro@proton.me>
2026-07-10 22:33:12 +0300

    ffmpeg_rtmp_pipe_in

--- include/encoder/provider/video_pipe/ffmpeg_rtmp/ffmpeg_rtmp_pipe_in.hpp
+++ include/encoder/provider/video_pipe/ffmpeg_rtmp/ffmpeg_rtmp_pipe_in.hpp
@@ -6,6 +6,7 @@ class ffmpeg_rtmp_pipe_in : public video_pipe_in {
 private:
     ffmpeg_rtmp_config _config;
     FILE *_pipe_in;
+    int _frame_size();
 public:
     ffmpeg_rtmp_pipe_in(ffmpeg_rtmp_config config);
     uint8_t *read(int size) override;
--- src/encoder/provider/video_pipe/ffmpeg_rtmp/ffmpeg_rtmp_pipe_in.cpp
+++ src/encoder/provider/video_pipe/ffmpeg_rtmp/ffmpeg_rtmp_pipe_in.cpp
@@ -1,5 +1,8 @@
 #include "encoder/provider/video_pipe/ffmpeg_rtmp/ffmpeg_rtmp_pipe_in.hpp"
+#include <cstdint>
 #include <cstdio>
+#include <chrono>
+#include <thread>
 
 ffmpeg_rtmp_pipe_in::ffmpeg_rtmp_pipe_in(ffmpeg_rtmp_config config){
     _config = config;
@@ -8,9 +11,22 @@ ffmpeg_rtmp_pipe_in::ffmpeg_rtmp_pipe_in(ffmpeg_rtmp_config config){
     _pipe_in = popen(command, "r");
 }
 
+int ffmpeg_rtmp_pipe_in::_frame_size() { return _config.frame_height * _config.frame_width  * 3; }
+
 uint8_t *ffmpeg_rtmp_pipe_in::read(int size) {
-    uint8_t *data=new uint8_t[size];
-    fread(data, 1, size, _pipe_in);
+    uint8_t *data=new uint8_t[size], *ptr;
+    int frame_size = _frame_size();
+    int frame_count = size / frame_size + (size % frame_size > 0 ? 1 : 0);
+    auto frame_duration = std::chrono::milliseconds(1000 / _config.fps);
+    auto next_frame_time = std::chrono::steady_clock::now();
+
+    for(int i = 0; i < frame_count; i++) {
+        ptr = data + i * frame_size;
+        fread(ptr, 1, frame_size, _pipe_in);
+        next_frame_time += frame_duration;
+        std::this_thread::sleep_until(next_frame_time);
+    }
+    
     return data;
 }
 
